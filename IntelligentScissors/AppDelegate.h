3ac02ecdcb48a77c34eca383aae66db84f3538d8
Chao Li <lichao0703@gmail.com>
2014-10-04 17:44:34 -0400

    Initial Commit

---
+++ IntelligentScissors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntelligentScissors
+//
+//  Created by Chao Li on 10/4/14.
+//  Copyright (c) 2014 Columbia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntelligentScissors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntelligentScissors
+//
+//  Created by Chao Li on 10/4/14.
+//  Copyright (c) 2014 Columbia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
